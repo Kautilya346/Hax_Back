c0ac9e35818a19e3fc2fa543e630c40c15883d91
Kautilya03 <23ucs613@gmail.com>
2025-01-24 00:09:53 +0530

    Multer-Cloudinary

---
+++ Utils/Cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloundinary} from "cloudinary"
+import fs from "fs"
+
+cloundinary.config({
+    cloud_name:process.env.CLOUD_NAME,
+    api_key:process.env.CLOUD_API_KEY,
+    api_secret:process.env.CLOUD_API_SECRET
+})
+
+const uploadOnCLoudinary=async (localFilePath)=>{
+    try {
+        if(!localFilePath){
+            return null
+        }
+
+        const response=await cloundinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+
+        fs.unlinkSync(localFilePath)
+        return response
+    } 
+    
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export {uploadOnCLoudinary}
